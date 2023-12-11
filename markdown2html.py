55dce1d9ad28aa888be710ca29d632b9a599939b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-12-11 13:24:03 -0800

    Added the markdown2html.py script
    This commit adds the markdown2html.py script that implements the basic features for converting Markdown files to HTML. The script checks the number of arguments provided and the existence of the Markdown file, displaying appropriate error messages and exiting with specific error codes if necessary. The script complies with PEP 8 standards and is ready for future developments.
    
    Functionalities implemented:
    
    - Checking the number of arguments
    
    - Checking the existence of the Markdown file
    
    - Error management and output with the appropriate error codes

---
+++ markdown2html.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+import os
+
+
+def main():
+    """
+    Main function that converts a Markdown file to HTML.
+
+    Reads the name of the Markdown file from the command line arguments,
+    checks if the file exists, and exits with an error if it doesn't.
+
+    Usage: ./markdown2html.py README.md README.html
+    """
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        exit(1)
+
+    markdown_file = sys.argv[1]
+
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        exit(1)
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
